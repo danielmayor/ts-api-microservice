826ffc31386d32420bff67484135fda48815235b
Daniel Mayor <cacique500@gmail.com>
2020-11-13 15:54:08 +0100

    Service restarts if container crashes & state preservation testing

--- api/index.js
+++ api/index.js
@@ -8,6 +8,31 @@ const app = express();
 const digestList = [];
 const digestDict = {};
 
+function updateFile(file, element) {
+  fs.writeFile(file, JSON.stringify(element), function (err) {
+    if (err) throw err;
+  });
+}
+
+fs.writeFile('digestDict.txt', '', { flag: 'wx' }, function (err) {
+    console.log('digestDict persistent storage ready');
+});
+fs.writeFile('digestList.txt', '', { flag: 'wx' }, function (err) {
+    console.log('digestList persistent storage ready');
+});
+
+fs.readFile('digestList.txt', 'utf8', function (err, dl_content) {
+  if (err) throw err;
+  if (dl_content) {
+    console.log(dl_content);
+  }
+});
+
+fs.readFile('digestDict.txt', 'utf8', function (err, dd_content) {
+  if (err) throw err;
+  console.log('digestDict ready');
+});
+
 app.use(bodyParser.json());
 app.set('json spaces', 2);
 
@@ -15,8 +40,27 @@ app.post('/messages', (req, res) => {
   // code to add a message and SHA256 hash digest...
   var crypto = require('crypto');
   const digest = crypto.createHash('sha256').update(req.body.message).digest('hex');
-  digestList.push(digest);
-  digestDict[digest] = req.body.message
+
+  if (digestDict.includes(digest)) {
+    console.log('key already exists!');
+  } else {
+    console.log('key does not exist: ' + digest);
+    digestList.push(digest);
+    digestDict[digest] = req.body.message
+    updateFile('digestList.txt', digestList);
+    updateFile('digestDict.txt', digestDict);
+  }
+
+  //fs.writeFile('digestList.txt', JSON.stringify(digestList), function (err) {
+  //  if (err) throw err;
+  //  console.log('digestList updated');
+  //});
+
+  //fs.writeFile('digestDict.txt', JSON.stringify(digestDict), function (err) {
+  //  if (err) throw err;
+  //  console.log('digestDict updated: key ' + req.body.message + ' saved!');
+  //});
+
   res.setHeader('Content-Type', 'application/json');
   res.status(201).send({'digest': digest});
 });
@@ -42,6 +86,7 @@ app.get('/messages/*', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send({'message': digestDict[hash_digest]});
   } else {
+    console.log(req.params[0] + ' hash does not exist!');
     res.sendStatus(404);
   }
 });
